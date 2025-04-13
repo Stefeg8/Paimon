ced3ae8736bfec2358267a608a42c79787a3c6e4
unknown <stevenshi2024@gmail.com>
2025-04-13 12:15:21 -0700

    takeoff simulation code

--- testing/takeoff_simulation.py
+++ testing/takeoff_simulation.py
@@ -27,6 +27,62 @@ def send_thrust(master, start_time, thrust, duration):
         )
         time.sleep(0.05)  # 20 Hz
 
+def simulate_takeoff_and_landing(master, start_time, takeoff_thrust=0.6, ramp_duration=3, hold_duration=2):
+    steps = int(ramp_duration / 0.05) 
+    thrust_values_up = [i * (takeoff_thrust / steps) for i in range(steps + 1)]
+    thrust_values_down = list(reversed(thrust_values_up))
+
+    # Ramp up
+    print("ramping up")
+    for thrust in thrust_values_up:
+        master.mav.set_attitude_target_send(
+            int((time.time() - start_time) * 1000),
+            master.target_system,
+            master.target_component,
+            type_mask=0b00000111,
+            q=[1, 0, 0, 0],
+            body_roll_rate=0,
+            body_pitch_rate=0,
+            body_yaw_rate=0,
+            thrust=thrust
+        )
+        time.sleep(0.05)
+
+    # Hover
+    print("hovering")
+    hover_end = time.time() + hold_duration
+    while time.time() < hover_end:
+        master.mav.set_attitude_target_send(
+            int((time.time() - start_time) * 1000),
+            master.target_system,
+            master.target_component,
+            type_mask=0b00000111,
+            q=[1, 0, 0, 0],
+            body_roll_rate=0,
+            body_pitch_rate=0,
+            body_yaw_rate=0,
+            thrust=takeoff_thrust
+        )
+        time.sleep(0.05)
+
+    # Ramp down
+    print("ramping down thrust")
+    for thrust in thrust_values_down:
+        master.mav.set_attitude_target_send(
+            int((time.time() - start_time) * 1000),
+            master.target_system,
+            master.target_component,
+            type_mask=0b00000111,
+            q=[1, 0, 0, 0],
+            body_roll_rate=0,
+            body_pitch_rate=0,
+            body_yaw_rate=0,
+            thrust=thrust
+        )
+        time.sleep(0.05)
+
+    print("hopefully touchdown")
+
 print("Initializing connection")
 master = mavutil.mavlink_connection("COM4", baud=57600)
 print("working...")
@@ -81,18 +137,12 @@ master.mav.command_long_send(
 time.sleep(1)
 print("OFFBOARD mode set")
 
-# Apply thrusts
-print("applying 0.25 thrust")
-send_thrust(master, start_time, thrust=0.25, duration=3)
-
-print("applying 0.5 thrust")
-send_thrust(master, start_time, thrust=0.5, duration=3)
-
-print("applying 0.75 thrust")
-send_thrust(master, start_time, thrust=0.75, duration=3)
-
-print("applying 0 thrust and initiating shutdown")
-send_thrust(master, start_time, thrust=0.0, duration=1)
+# Takeoff simulation
+# Pass takeoff_thrust as the 3rd argument to get a custom thrust value. Default is 0.6 in the function
+takeoff_thrust = 0.5 # Ramp up slowly from 0.5 to 0.65 in your testing. You can adjust this value as needed. do 0.02 increments ig
+ramp_duration = 3 # Adjust this value as needed. its how long it takes for the drone to go up from 0 thrust to takeoff thrust. don't really need to change this though
+simulate_takeoff_and_landing(master, start_time, takeoff_thrust, ramp_duration)
+#simulate_takeoff_and_landing(master, start_time) # this is the default function. you can use this if you want. 
 
 # Disarm the drone
 print("disarming")
